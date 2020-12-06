321c4ed530d27923faab99825414e1cb99d266f8
Ryan Swann <RyanSwann1992@gmail.com>
2020-12-06 20:23:19 +0000

    Corrected Key & Value system. Now acts according to std::unordered_map

--- CS_Fundamentals/UnorderedMap.h
+++ CS_Fundamentals/UnorderedMap.h
@@ -3,29 +3,38 @@
 #include <assert.h>
 #include <array>
 
+//https://www.youtube.com/watch?v=LsjFAx-dG5I
+//https://www.cs.usfca.edu/~galles/visualization/OpenHash.html
+
+//https://stackoverflow.com/questions/50073376/how-does-stdunordered-map-store-and-compare-its-keys-to-achieve-fast-access-to
+
 template <class Value>
 struct UnorderedMapNode
 {
-	UnorderedMapNode(const Value& data)
-		: data(data),
+	UnorderedMapNode(int key, const Value& data)
+		: key(key),
+		data(data),
 		next(nullptr)
 	{}
 	UnorderedMapNode(const UnorderedMapNode&) = delete;
 	UnorderedMapNode& operator=(const UnorderedMapNode&) = delete;
 	UnorderedMapNode(UnorderedMapNode&& orig) noexcept
-		: data(std::move(orig.data)),
+		: key(orig.key),
+		data(std::move(orig.data)),
 		next(orig.next)
 	{
 		orig.next = nullptr;
 	}
 	UnorderedMapNode& operator=(UnorderedMapNode&& orig) noexcept
 	{
+		key = orig.key;
 		data = std::move(orig.data);
 		next = orig.next;
 
 		orig.next = nullptr;
 	}
 	
+	int key;
 	Value data;
 	UnorderedMapNode* next;
 };
@@ -104,21 +113,51 @@ public:
 		int hashKey = getHashKey(key);
 		if (!m_buckets[hashKey])
 		{
-			m_buckets[hashKey] = new UnorderedMapNode<Value>(data);
+			m_buckets[hashKey] = new UnorderedMapNode<Value>(key, data);
 		}
 		else
 		{
+			UnorderedMapNode<Value>* previousNode = m_buckets[hashKey];
 			UnorderedMapNode<Value>* node = m_buckets[hashKey];
-			while (node->next)
+			bool addElement = true;
+			while (node)
 			{
+				if (node->key == key)
+				{
+					addElement = false;
+					break;
+				}
+
+				previousNode = node;
 				node = node->next;
 			}
 
-			node->next = new UnorderedMapNode<Value>(data);
+			if (addElement)
+			{
+				previousNode->next = new UnorderedMapNode<Value>(key, data);
+			}
 		}
 	}
 
-	size_t remove(int key)
+	UnorderedMapNode<Value>* find(int key)
+	{
+		int hashKey = getHashKey(key);
+		if (m_buckets[hashKey])
+		{
+			UnorderedMapNode<Value>* node = m_buckets[hashKey];
+			while (node)
+			{
+				if (node->key == key)
+				{
+					return node;
+				}
+			}
+		}
+
+		return nullptr;
+	}
+
+	size_t emptyBucket(int key)
 	{
 		size_t nodesDeleted = 0;
 		int hashKey = getHashKey(key);
@@ -134,6 +173,42 @@ public:
 
 		m_buckets[hashKey] = nullptr;
 		return nodesDeleted;
+	}
+
+	void remove(int key)
+	{
+		int hashKey = getHashKey(key);
+		assert(m_buckets[hashKey]);
+
+		UnorderedMapNode<Value>* previousNode = m_buckets[hashKey];
+		UnorderedMapNode<Value>* node = m_buckets[hashKey];
+		bool elementDeleted = false;
+		while (!elementDeleted && node)
+		{
+			if (node == m_buckets[hashKey])
+			{
+				UnorderedMapNode<Value>* nextNode = node->next;
+				delete node;
+				if (nextNode)
+				{
+					m_buckets[hashKey] = nextNode;
+					elementDeleted = true;
+				}
+			}
+			else
+			{
+				UnorderedMapNode<Value>* nextNode = node->next;
+				if (node->key == key)
+				{
+					delete node;
+					elementDeleted = true;
+				}
+
+				previousNode->next = nextNode;
+			}
+
+			node = node->next;
+		}
 	}
 
 	void clear()
